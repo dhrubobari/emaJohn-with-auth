ee1ff8854ab408744c6104e58d3292e5fa618ec3
dhrubobari <bari.dhrubo@gmail.com>
2022-03-23 20:58:50 +0600

    first commit

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
 
-const Product = ({product, handleAddToCart}) => {
-    // const {product, handleAddToCart} = props;
-    const { name, img, seller, price, ratings } = product;
-    
+const Product = (props) => {
+    const {name, category, img, price, ratings} = props.product;
+    console.log(props)
     return (
-        <div className='product'>
-            <img src={img} alt=""></img>
-            <div className='product-info'>
-                <p className='product-name'>{name}</p>
-                <p>Price: ${price}</p>
-                <p><small>Seller: {seller}</small></p>
-                <p><small>Ratings: {ratings} stars</small></p>
+        <div className="product">    
+            <img src={img} alt="" />
+            <div className="product-info">
+            <p className="product-name">{name}</p>
+            <p>price: ${price}</p>
+            <p><small>category: {category}</small></p>
+            <p><small>ratings: {ratings} stars</small></p>
             </div>
-            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
-                <p className='btn-text'>Add to Cart</p>
-                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
-            </button>
+            
+            <button onClick={() => props.handleAddToCart(props.product)} className="button-cart">
+            <p>Add to Cart</p><FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></button>
+        
         </div>
     );
 };
